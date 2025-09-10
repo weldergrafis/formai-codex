c058fd510f4ee365350550bc850e5802f4bf4274
Welder Knaf <welder@grafis.com.br>
2025-09-10 11:01:27 -0300

    Sql server database setup

--- FormAI.Api/FormAI.Api/Data/AppDbContext.cs
+++ FormAI.Api/FormAI.Api/Data/AppDbContext.cs
@@ -8,4 +8,26 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
 {
     // DbSet for storing photos
     public DbSet<Photo> Photos => Set<Photo>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Comentário: só aplica se ninguém configurou ainda (ex.: DI)
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Comentário: fonte da verdade ÚNICA
+            var connectionString = "Server=tcp:formai-sql-server.database.windows.net,1433;Initial Catalog=formai;Persist Security Info=False;User ID=formai;Password=Test@ndo123456;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            optionsBuilder.UseSqlServer(connectionString, sql => sql.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null));
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configura todas as entidades para usar o nome da classe como nome da tabela
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            modelBuilder.Entity(entity.Name).ToTable(entity.ClrType.Name);
+        }
+    }
 }
---
+++ FormAI.Api/FormAI.Api/Migrations/20250910133545_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormAI.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Photo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photo", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photo");
+        }
+    }
+}
